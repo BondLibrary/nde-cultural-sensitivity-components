241a6f2266d93d98817521b26ec277a4bb20d98a
Bond University Library Services <144414293+BondLibrary@users.noreply.github.com>
2025-08-21 08:44:11 +1000

    Add files via upload

---
+++ content-advice-indication.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'custom-content-advice-indication',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content-advice-indication.component.html',
+  styleUrls: ['./content-advice-indication.component.scss']
+})
+export class ContentAdviceIndicationComponent {
+  @Input() hostComponent!: any;
+
+  hasLds06 = false;
+
+  ngOnInit() {
+    //console.log('ContentAdviceIndicationComponent ngOnInit:'); // Uncomment these lines to see the hostComponent in the console
+    //console.log(this.hostComponent);
+
+    const display = this.hostComponent?.display;
+    if (display?.lds06) {
+      this.hasLds06 = true;
+    }
+  }
+}
